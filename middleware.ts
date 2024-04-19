40aae1012168da219ccc12c948d01c4b4a4ec7a5
ShawSpring <chunlaixiao@163.com>
2024-04-20 02:04:18 +0800

    over

--- middleware.ts
+++ middleware.ts
@@ -7,4 +7,15 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
- 
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // You can also set request headers in NextResponse.rewrite
+  const response = NextResponse.next();
+
+  // Set a new response header `x-hello-from-middleware2`
+  response.headers.set('x-hello-from-middleware2', 'hello');
+  return response;
+}
