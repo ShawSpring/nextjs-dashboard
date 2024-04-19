e80caf9d0418273df1a1c001c80f4c004b0d333d
ShawSpring <chunlaixiao@163.com>
2024-04-20 02:04:18 +0800

    over

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,16 +1,38 @@
-//import {useState} from 'react';
-import { Metadata } from 'next';
-export const metadata: Metadata = {
-  title: 'customers', // 自动嵌入模板中 template: '%s | Acme Dashboard',
-};
-
-function page() {
+'use client';
+import { useEffect } from 'react';
+//* client中 不能使用 Metadata, 改用js手动设置
+// import { Metadata } from 'next';
+// export const metadata: Metadata = {
+//   title: 'customers', // 自动嵌入模板中 template: '%s | Acme Dashboard',
+// };
+import Head from 'next/head';
+function Page() {
   //const [data, setData] = useState([]);
+  function handleFetch() {
+    fetch('/api/data')
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log('Fetch error: ', error);
+      });
+  }
+  useEffect(() => {
+    document.title = 'customers| Acme Dashboard';
+  }, []);
+
   return (
-    <>
+    <div>
       <p>customers</p>
-    </>
+      <button
+        onClick={handleFetch}
+        className="rounded-md bg-cyan-500 px-1 py-2"
+      >
+        fetch
+      </button>
+    </div>
   );
 }
 
-export default page;
+export default Page;
